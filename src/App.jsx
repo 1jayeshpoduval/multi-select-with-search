65dda22229c1c0ecd608530085aace54151c8991
Jayesh Poduval <jayeshpoduval31@gmail.com>
2024-12-16 11:14:23 +0300

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <section className="h-screen flex items-center justify-center">
       <div className="w-[90%] mx-auto flex flex-col gap-4 items-center justify-center">
+        <h1 className="text-lg font-medium text-neutral-800">
+          Multi-select with search
+        </h1>
         <MultiSelectSearch />
       </div>
     </section>
