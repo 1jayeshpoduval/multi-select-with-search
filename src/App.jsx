bf9edd9077497905ee0dc922666fc1fff6825681
Jayesh Poduval <jayeshpoduval31@gmail.com>
2024-12-16 20:15:23 +0300

    minor refinements

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
 import MultiSelectSearch from "./MultiSelectSearch";
 
 function App() {
   return (
     <section className="h-screen">
       <div className="relative w-[90%] h-full mx-auto flex flex-col items-center justify-center space-y-4">
-        <div className="w-full flex flex-col items-center justify-center gap-4">
-          <h1 className="font-medium text-xl text-neutral-800 tracking-tight">
+        <div className="w-full flex flex-col items-center justify-center gap-8">
+          <h1 className="font-medium text-xl text-neutral-800 tracking-[-0.01em]">
             Multi-select with search
           </h1>
           <MultiSelectSearch />
         </div>
         <div className="text-neutral-400 text-center absolute bottom-6">
-          Built by{" "}
+          Built by {""}
           <a
-            className="hover:text-neutral-900 transition-colors underline"
+            className="hover:text-neutral-900 transition-colors"
             href="https://x.com/1jayeshpoduval"
             target="_blank"
             rel="noreferrer"
